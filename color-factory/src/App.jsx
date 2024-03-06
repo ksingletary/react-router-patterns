d102830bcc1875bb48634a02eac84bdcdb63a772
ksingletary <keithsingletary@gmail.com>
2024-03-06 15:58:56 -0500

    react router patterns

---
+++ color-factory/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Link} from 'react-router-dom'
+import RoutesList from './RoutesList'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+      <Link to="/colors">Home </Link><Link to="/colors/new">Add a color</Link>
+        <RoutesList/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ color-factory/src/ColorsIndex.jsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+import ColorDetail from './ColorDetail'
+const ColorsIndex = ({colors}) =>{
+
+
+  return (
+    <>
+    {/* <Link to="/colors/new"><button>Add a color</button></Link> */}
+    <h2 className='text-3xl font-bold'>This is a list of the colors</h2>
+    {colors.map(color => (<Link key={color.id} to={color.colorName.toLowerCase()}> {color.colorName} </Link>   ) ) }
+
+    </>
+  )
+}
+
+export default ColorsIndex;
